fec777a89b2ff450cf111b195cf51f32f2380030
sants1304h <san1203scs@gmail.com>
2023-06-06 18:55:31 -0500

    Video 3 Curso C++

---
+++ Entrada_Datos/Entrada_Datos.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    cout<<"Digite un numero: \n";
+    cin>>numero;
+
+    cout<<"El numero que digito es: "<<numero;
+
+    return 0;
+}
