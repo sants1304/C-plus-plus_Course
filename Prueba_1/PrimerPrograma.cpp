c00ff2787c319b79ef25d4bcd15c5a0166f44869
sants1304h <san1203scs@gmail.com>
2023-06-06 17:34:09 -0500

    Video 1 Curso C++

---
+++ Prueba_1/PrimerPrograma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    // Sin usar el namespace std: 
+    // std::count<<"Hola Mundo, Eres cule capo San";
+
+    //endl = Salto de línea
+    //cout<<"Hola Mundo, Eres cule capo San"<<endl; 
+
+    // \n = Salto de línea
+    cout<<"Hola Mundo, Eres cule capo San\n"; 
+
+    return 0;
+
+}
