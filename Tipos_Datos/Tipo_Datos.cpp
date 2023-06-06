bae9f1c4d1dbf57fd5fcd5e1cef4fce1c4cf87bf
sants1304h <san1203scs@gmail.com>
2023-06-06 18:28:53 -0500

    Video 2 Curso C++

---
+++ Tipos_Datos/Tipo_Datos.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int entero = 15; 
+    float decimal = 19.4;           //32 bits
+    double doble = 234.240959923;   //64 bits
+    char caracter = '@';
+
+    cout<<entero<<endl;
+    cout<<decimal<<endl;
+    cout<<doble<<endl;
+    cout<<caracter<<endl;
+
+    return 0; 
+}
