95791df42a3d0b4047b75c1993460003079eac9c
sants1304h <san1203scs@gmail.com>
2023-06-07 12:20:12 -0500

    C++ Course Codes

---
+++ Bloque_2/Ejercicios/Ejercicio_Expresion_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    float num_1, num_2, num_3, num_4, resultado = 0;
+
+    cout<<"Digite el numero a: "; cin>>num_1;
+    cout<<"Digite el numero b: "; cin>>num_2;
+    cout<<"Digite el numero c: "; cin>>num_3;
+    cout<<"Digite el numero d: "; cin>>num_4;
+
+    resultado = (num_1 + num_2)/(num_3 + num_4);
+
+    cout<<"\nEl resultado es: "<<resultado<<endl;
+
+
+    return 0;
+
+}
