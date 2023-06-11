d1930338beaa0a6cb2d0a10343e4c50beeaf6231
sants1304h <san1203scs@gmail.com>
2023-06-11 13:38:27 -0500

    Video 40

---
+++ Caracteres/Cadena_Caracteres.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>  
+//Cadena caracteres. https://cplusplus.com/reference/cstring/
+
+using namespace std;
+
+int main(){
+    
+    char palabra[] = "Santiago"; 
+    char nombre_1[20];
+
+    cout<<"Introduzca su nombre: ";
+    cin.getline(nombre_1,20,'\n');  //cin.getline(varible,tamaño,comando_salida)
+
+    cout<<palabra<<endl;
+    cout<<nombre_1<<endl;
+    
+    system("pause");
+    return 0;
+}
